380201b2e5bb4c866d5a3991439fbe4713249305
patrikkeller <patrik.keller@gmail.com>
2011-04-14 22:41:55 +0200

    corrections

--- code-patrik/v1/plotinter.m
+++ code-patrik/v1/plotinter.m
@@ -3,7 +3,7 @@ function [ ok ] = plotinter( x,y,value )
 %   Detailed explanation goes here
 
 line=[x x;y y; 0 value];
-set(line,'LineWidth',2)
+%set(line,'LineWidth',2)
 plot3(line(1,:),line(2,:),line(3,:),'r')
 
 ok=1
