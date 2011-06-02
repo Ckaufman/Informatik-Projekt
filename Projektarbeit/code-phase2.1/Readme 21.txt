62ae3b957e8d0c16da3d4500e054ed5f33dea1b6
patrikkeller <patrik.keller@gmail.com>
2011-06-02 09:49:36 +0200

    phase2.1 mit funktionierendem AutoButton, Jacobi

---
+++ Projektarbeit/code-phase2.1/Readme 21.txt
@@ -0,0 +1,5 @@
+Version 2.1 v3
+
+2D einstellen von Stützstellen manuell
+
+... mit integriertem Auto button
--- Projektarbeit/code-phase2.1/f.m
+++ Projektarbeit/code-phase2.1/f.m
@@ -1,31 +1,31 @@
-function [ y ] = f( x,a )
-% f gibt den linear interpolierten Wert an der Position x zwischen den
-% Stützstellen in a zurück
-% a als Spaltenvektor
-
-aufl=101;                   % Anzahl Werte zwischen 1 und letzter Stützstelle
-% Anzahl Stützstellen, Spaltenvektor
-n=size(a,1); 
-if n ==1
-    n=size(a,2);
+function [ y ] = f( x,a,n )
+% a enthält beliebig viele Stützstellen
+% Die Funktion gibt den zwischen den Stützstellen linear interpolierten Funtionswert zurück
+% wobei n die Auflösung angibt, es gilt
+% x <= n
+% x=0 => y= a(1)
+% x=n => y= a("letzter")
+% erste Stützstelle = a(1), letzte = a(size(a,1))
+if size(a,2)>size(a,1)
+	a=a';
 end
-              
-abstand= (aufl-1)/(n-1);    % Abstand zwischen den Stützstellen
-stuetzpos=0:abstand:100;    % x Positionen der Stützstellen
 
-if (x>0) && (x<100)
-    %linear zwischen den Werten des a-Vektors interpolieren
-    lowStuetzPos = floor((x/(abstand))+1);   % 1. Stützstelle
-    highStuetzPos= lowStuetzPos+1;           % 2. Stützstelle
-    A= a(lowStuetzPos);                      % Wert der 1. Stützstelle
-    B= a(highStuetzPos);                     % Wert der 2. Stützstelle
-    y= interpolate((x-(lowStuetzPos-1)*abstand),A,B,abstand);
-   
-elseif (x==100)
-    y=a(n);
+if size(a,1)==2
+	a1=a(1);
+	a2=a(2);
+	y= a1+x/n * (a2-a1);
 else
-    y=a(1);
-end
-y;
+	dimension=size(a,1);            % Anzahl übergebene Stützstellen
+	parts=dimension-1;              % 0 bis n ist in (abh. von dimension) "parts" lineare Bereiche
+	ioben  = ceil (x/n*parts)+1;    % i-te Stützstelle oberhalb der x Position
+	iunten = floor(x/n*parts)+1;
+	% relative Position
+	rel = rem(x,parts);
+	if rel == 0
+	    y=a(iunten);                % x entspricht einer Stützstelle
+	else
+		y= a(iunten) + rel/parts *(a(ioben) - a(iunten));
+	end
+		
 end
 
